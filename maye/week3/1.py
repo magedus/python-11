4fe97774ad899f71a8c98d32d56d73cd98ef7b4e
maye123456 <237252806@qq.com>
2018-04-07 16:56:17 +0800

    aaa

---
+++ maye/week3/1.py
@@ -0,0 +1,14 @@
+'''
+    import random
+    lst=[[1,2,3],["x",7,8],[9,6]]
+    lst1=random.choice(lst)
+    if 'x' in lst1:
+        print(lst1)
+        print("存在",1)
+    else:
+        print(lst1)
+        print("不存在",0)
+    '''
+
+str1='this is my desktop,dont open any file!'
+print(str1.count('i'))
