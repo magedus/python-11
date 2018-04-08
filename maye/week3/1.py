486bb17f593ade2f0eea803f5db3f4ca56a6d6e0
maye123456 <237252806@qq.com>
2018-04-08 22:16:28 +0800

    aaa

--- maye/week3/1.py
+++ maye/week3/1.py
@@ -10,6 +10,10 @@
         print("不存在",0)
     '''
 
-str1="After final machining, all surfaces shall be examined by the wet magnetic particle method in accordance with ASME VIII, Division 1, Appendix 7.    If magnetic particle examination is not possible, then liquid penetrant examination shall be performed in accordance with ASME VIII, Division 1, Appendix 7.    6.13.2.4          Replace this clause with:   Nodular iron castings shall be produced in accordance with ASTM A395/A395M, or ASTM A536/A536M provided it meets or exceeds the material requirements of ASTM A395 as indicated by appropriate testing"
+from collections import namedtuple
+str1="After final machining, all  all surfaces shall be examined by the wet magnetic particle method in accordance with ASME VIII, Division 1, Appendix 7.    If magnetic particle examination is not possible, then liquid penetrant examination shall be performed in accordance with ASME VIII, Division 1, Appendix 7.    6.13.2.4          Replace this clause with:   Nodular iron castings shall be produced in accordance with ASTM A395/A395M, or ASTM A536/A536M provided it meets or exceeds the material requirements of ASTM A395 as indicated by appropriate testing"
 str2=str1.split()
-print(str2.count("in"))
+Point=namedtuple('P','name,num')
+for a in str2:
+    p1=Point(a,str2.count(a))
+    print(p1)
