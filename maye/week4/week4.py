f35058ecf93bbddeeb553577268975fc96d507b6
maye123456 <237252806@qq.com>
2018-04-12 20:58:55 +0800

    aaa

--- maye/week4/week4.py
+++ maye/week4/week4.py
@@ -3,17 +3,18 @@ lst=[]
 s=0
 for i in range(20):
     a = random.random()
-    n = random.randint(0, 320)
+    n = random.randint(0, 30)
     s=int(a*10**n)
     lst.append(s)
 lst.sort()
-print(lst[0:3])
+print(lst[-3:])
 
 print('please type five numebers')
 a=input('>>>')
-for i in range(5):
+for i in range(5//2+1):
     if a[i]!=a[-i-1]:
         print('no')
         break
 else:
     print('yes')
+
