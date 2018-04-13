29cbb30eb4ed82d8740c7f5ca325fea5a501e5da
MX4Pro <12753869@qq.com>
2018-04-13 11:01:22 +0800

    week4 homework

---
+++ chenguowen/week4/palindromic.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# 判断是否是回文数
+
+def palindromic():
+    while True:
+        num = input('请输入一个5位正整数: ')
+    
+    #判断输入的是否是5位正整数
+        if num.isdigit() and len(num) == 5:
+            
+            #判断是否是回文数
+            if num == num[::-1]:
+                print('你输入的{}是回文数.'.format(num))
+                break
+            else:
+                print('你输入的{}不是回文数.'.format(num))
+                break
+        else:
+            print('你输入的{}不是一个5位正整数，请重新输入: '.format(num))
+
+        
+palindromic()
