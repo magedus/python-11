ba1d46021fdb034deeb6f6d6fce5aa65820c4478
gvanrossum <dbawww@126.com>
2018-04-15 17:36:56 +0800

    answer

---
+++ Answers/week1/job1.py
@@ -0,0 +1,41 @@
+# 1、打印出100以内的斐波那契数列，使用2种方法实现
+
+def fibonacci_1(num: int):
+    if num == 0:
+        return 0
+    elif num == 1:
+        return 1
+    a = 0
+    b = 1
+    for i in range(num):
+        a, b = b, a+b
+    print(a, b)
+
+
+def fibonacci_2(num: int):
+    if num <= 1:
+        return num
+
+    return fibonacci_2(num-1) + fibonacci_2(num-2)
+
+
+# 2、使用 Python 实现随机生成 200 无重复激活码（或者优惠券），字符串长度大于5以上
+import string
+import random
+
+def gen_promotion_code(num: int, length: int):
+    """
+
+    :param num:     生成的优惠券数量
+    :param length:  生成的优惠券长度
+    :return:
+    """
+    # string模块引入字符串和数字作为生成基数
+    sd = string.ascii_letters + string.digits
+    s = set()
+    while len(s) < num:
+        promotion_code = ''.join(random.sample(sd, length))
+        s.add(promotion_code)
+    return s
+
+gen_promotion_code(200, 9)
---
+++ Answers/week4/job4.py
@@ -0,0 +1,34 @@
+# 1、问题描述：一个5位数，判断它是不是回文数。
+
+def is_palindromic(num: int):
+    s1 = str(num)
+    s2 = s1[::-1]
+    if s1 == s2:
+        return '{} 是回文数'.format(num)
+    return '{} 不是回文数'.format(num)
+
+def is_palindromic_2(num: int):
+    s1 = str(num)
+    length = len(s1)
+    for i in range(length//2):
+        if s1[i] != s1[length-i-1]:
+            return '{} 不是回文数'.format(num)
+    return '{} 是回文数'.format(num)
+
+
+
+
+# 2、随机生成20个数字，并且筛选出其中最大的3个数
+import string
+import random
+
+def gen_random_num(nums):
+    s1 = string.digits
+    lst = []
+    for i in range(nums):
+        num = int(''.join(random.sample(s1, 5)))
+        lst.append(num)
+    lst.reverse()
+    return lst[-3:]
+
+
