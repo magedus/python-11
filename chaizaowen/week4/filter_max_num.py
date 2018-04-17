a76e91b3e261792394ad6208a964632bf117653b
jowinchai <chaizaowen@163.com>
2018-04-17 08:11:36 +0800

    filter_max_num.py changed

--- chaizaowen/week4/filter_max_num.py
+++ chaizaowen/week4/filter_max_num.py
@@ -1,21 +1,24 @@
 import random
 
-def fil(nums,target=[]):
+def fil(nums):
     '''
     this is function is used to filter max number.
 
     num: list
     return the maximum number of three:
     '''
+    ret = []
     print(nums)
     for i in range(len(nums)):
         flag = False
         for j in range(len(nums)-i-1):
-            if nums[j] > nums[j+1]:
+            if nums[j] < nums[j+1]:
                 nums[j],nums[j+1] = nums[j+1],nums[j]
                 flag = True
         if not flag:
             break
-    return nums[-1],nums[-2],nums[-3]
+    print(nums)
+    ret = nums[0:3]
+    return ret
 
 print(*fil([random.randint(1,20) for _ in range(20)]))
