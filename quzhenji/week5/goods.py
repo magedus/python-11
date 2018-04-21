19191f01634c0d90b1e99f5c589930e74bf656c7
Linran <zhenji_qu@126.com>
2018-04-21 13:38:11 +0800

    update goods.py

--- quzhenji/week5/goods.py
+++ quzhenji/week5/goods.py
@@ -1,13 +1,25 @@
 import random
 
+def goods():
+    Socks = ['Sock']*10
+    shoes = ['shoes']*20
+    Slipper = ['Slipper']*30
+    Necklace = ['Necklace']*40
 
-Socks = ['Sock']*10
-shoes = ['shoes']*20
-Slipper = ['Slipper']*30
-Necklace = ['Necklace']*40
+    all_goods = Socks + shoes + Slipper + Necklace
 
-all_goods = Socks + shoes + Slipper + Necklace
+    return random.choice(all_goods)
 
-print(all_goods)
-print('*'*200)
-print(random.choice(all_goods))
+dict1 = {}
+goods_lst = []
+for _ in range(100):
+    goods_lst.append(goods())
+
+#print(goods_lst.count("Sock"))
+for _ in range(4):
+    dict1['Sock'] = goods_lst.count("Sock")
+    dict1['shoes'] = goods_lst.count("shoes")
+    dict1['Slipper'] = goods_lst.count("Slipper")
+    dict1['Necklace'] = goods_lst.count("Necklace")
+
+print(dict1)
