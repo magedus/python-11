31227658ab4bd618a91b3c97ca6a4aac77647cc1
gaoyp27 <gaoyp27@126.com>
2018-04-22 22:17:25 +0800

    gaoyp27 week5

--- gaoyp27/week2/fib.py
+++ gaoyp27/week2/fib.py
@@ -2,19 +2,19 @@
 # Python 斐波那契数列实现
 
 # 获取用户输入数据
-nterms = int(input("你需要几项？"))
+num = int(input("你需要几项？"))
 
 n1=0
 n2=1
 count=2
 
-if nterms<=0:
+if num<=0:
     print("请输入一个正整数! ")
-elif nterms==1:
+elif num==1:
     print("斐波那契数列：%d"%n1)
 else:
     print('斐波那契数列：%d,%d' %(n1,n2),end=",")
-    while count<nterms:
+    while count<num:
         nth=n1 + n2
         print(nth,end=",")
         n1=n2
@@ -65,3 +65,27 @@ else:
             print(nth,end=",")
         n1=n2
         n2=nth
+
+#求斐波那契数列的第n项的
+# 获取用户输入数据
+num = int(input("你需要第几项？"))
+
+n1=0
+n2=1
+count=2
+
+if num<=0:
+    print("请输入一个正整数! ")
+elif num==1:
+    print("斐波那契数列：%d"%n1)
+else:
+    print('斐波那契数列：%d,%d' %(n1,n2),end=",")
+    while count<=num:
+        nth=n1 + n2
+        n1=n2
+        n2=nth
+        count+=1
+    if count==num:
+        print(nth,end=",")
+
+
