554eb68f3e10675cf57c2490c4410fdf0bdcc57b
awalysmile <awalysmile@qq.com>
2018-04-24 17:22:18 +0800

    修改了一下

--- ChenShiFei/week6/jobweek6.py
+++ ChenShiFei/week6/jobweek6.py
@@ -5,24 +5,24 @@ print(b)
 
 
 
-# # 输入一个英文句子,翻转句子中的单词顺序,但单词内字符的顺序不变
-#
-#
-# def rev_sentence(s: str):
-#     s_list = s.split()
-#     s_list.reverse()
-#     rev_s = ' '.join(s_list)
-#     return print(rev_s)
-#
-#
-# s1 = "I love everyone in magedu , especially the '不动教练' and MissLi"
-# rev_sentence(s1)
-#
-#
-# def rev_sentence2(s:str):
-#     s = ' '.join(reversed(s.split()))
-#     return s
-#
-#
-# s = 'magedu is a great group for thoes who want to learn'
-#print(rev_sentence2(s))
+# 输入一个英文句子,翻转句子中的单词顺序,但单词内字符的顺序不变
+
+
+def rev_sentence(s: str):
+    s_list = s.split()
+    s_list.reverse()
+    rev_s = ' '.join(s_list)
+    return print(rev_s)
+
+
+s1 = "I love everyone in magedu , especially the '不动教练' and MissLi"
+rev_sentence(s1)
+
+
+def rev_sentence2(s:str):
+    s = ' '.join(reversed(s.split()))
+    return s
+
+
+s = 'magedu is a great group for thoes who want to learn'
+print(rev_sentence2(s))
