8813e81e8ed3a7f9f8ea127f3f8766d60cb60ac5
lsclpangpang <80654925@qq.com>
2018-04-25 16:59:02 +0800

    huiwenshu.py

--- lipangpang/week2/激活码.py
+++ lipangpang/week2/激活码.py
@@ -1,1 +1,4 @@
 print("\n".join([__import__("hashlib").md5(str(__import__("uuid").uuid1()).encode("utf-8")).hexdigest() for x in range(200)]))
+
+
+print("\n".join([__import__("hashlib").md5(str(__import__("uuid").uuid1()).encode("utf-8")).hexdigest() for x in range(200)]))
