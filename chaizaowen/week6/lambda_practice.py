059bc92ee25b9b1eacdf1502a8ae4fc647a38286
jowinchai <chaizaowen@163.com>
2018-04-29 09:43:17 +0800

    practice changed

--- chaizaowen/week6/lambda_practice.py
+++ chaizaowen/week6/lambda_practice.py
@@ -1,3 +1,5 @@
 
 
-(lambda i,j:[{i[0]:j[0],j[0]:j[1]}])((('a'),('b')),(('c'),('d')))
+print((lambda i,j:[{i[0]:j[0],j[0]:j[1]}])((('a'),('b')),(('c'),('d'))))   #dest is [{'a':'c'｝,{'c':'d'}]
+
+print((lambda i,j:[dict(zip(i,j))])((('a'),('b')),(('c'),('d'))))   #dest is [{'a': 'c', 'b': 'd'}]
