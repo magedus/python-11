68f28334887ddbf14a57bc20aa52c28e16e9bbb6
maming <maming@mamingdeMacBook-Air.local>
2018-04-30 23:27:16 +0800

    modify new_list.py

--- maming/week6/new_list.py
+++ maming/week6/new_list.py
@@ -1,6 +1,6 @@
 dtuple=(('a'),('b')),(('c'),('d'))
 
 def func(tup):
-    return list(map(lambda x,y:dict(zip(x,y)),tup[0],tup[1]))  
+    return list(map(lambda x,y:dict(zip(x,y)),*tup))  
 
 print(func(dtuple))
