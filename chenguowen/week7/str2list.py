b94df5877f4f630ed869aabe49b3f147b2b6cddc
unknown <guowen.chen>
2018-05-14 20:45:26 +0800

    week7 homework

---
+++ chenguowen/week7/str2list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def str2list():
+    """
+
+    请将 "1,2,3"，变成 ["1","2","3"]
+    """
+    str1 = "1,2,3"
+    lst1 = str1.split(',')
+    print(lst1)
+
+str2list()
