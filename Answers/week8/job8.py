2f1a6e786fb23c1a99ccb843c008618f9ab0a6bc
pluto <kangdingyong@jinkejoy.com>
2018-05-19 19:46:32 +0800

    a

--- Answers/week8/job8.py
+++ Answers/week8/job8.py
@@ -2,6 +2,12 @@
 
 
 def find_sub(s1: str, s2: str):
+    """
+    直接使用字符串索引方式
+    :param s1:
+    :param s2:
+    :return:
+    """
     if not s1 or not s2:
         raise ValueError('input not allow empty string')
     try:
@@ -11,8 +17,25 @@ def find_sub(s1: str, s2: str):
     return i
 
 
-print(find_sub('11', '1eee'))
+def find_sub2(s1: str, s2: str):
+    """
+    对目标字符串进行循环，然后索引进行比较
+    :param s1:
+    :param s2:
+    :return:
+    """
+    if not s1 or not s2:
+        raise ValueError('input not allow empty string')
+    c = s2[0]
+    s2_length = len(s2)
+    for i, s in enumerate(s1):
+        if s == c:
+            if s1[i: i+s2_length] == s2:
+                return i
 
+
+print(find_sub('11', '1eee'))
+print(find_sub2('weqr', 'e'))
 
 # 给定一个整型列表，请实现从其中找出2个数的和为某一个指定的值
 # 如：lst =[1,5,2,7,4,9]，指定的目标值为11，可以从中找出 2和9之和为11
@@ -20,6 +43,12 @@ print(find_sub('11', '1eee'))
 
 
 def find_dst_value(target: int, targets: list):
+    """
+    time complexity O(N^2)
+    :param target:
+    :param targets:
+    :return:
+    """
     length = len(targets)
     for i in range(length):
         for j in range(i, length):
