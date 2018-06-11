3cf212a049493f426b1726de1e9a95c1139e4257
pluto <kangdingyong@jinkejoy.com>
2018-06-11 17:16:09 +0800

    fix format

--- Answers/week11/job11.py
+++ Answers/week11/job11.py
@@ -2,6 +2,7 @@
 # stack 是先进后出数据结构 进：1->2->3  出：<-1<-2<-3
 # queue 是先进先出数据结构 进：1->2->3  出：<-3<-2<-1
 
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -18,7 +19,6 @@ class Stack:
     def __init__(self):
         self.head = None
 
-
     def append(self, data):
         node = Node(data)
         node.next = self.head
@@ -33,15 +33,14 @@ class Stack:
         return tmp.value
 
 
- # test
-l1 = [1,5,3,2,5,6]
+# test
+l1 = [1, 5, 3, 2, 5, 6]
 stack = Stack()
 for x in l1:
     stack.append(x)
 
 for y in l1:
     print(stack.pop())
-
 
 
 # 2、打印出N对合理的括号组合。
@@ -60,18 +59,20 @@ def print_parenthesis(output, opend, close, pairs):
     if opend == pairs and close == pairs:
         print(output)
     else:
-        if opend<pairs:
-            print_parenthesis(output+'(', opend+1, close, pairs)
-        if close<opend:
-            print_parenthesis(output+')', opend, close+1, pairs)
+        if opend < pairs:
+            print_parenthesis(output + '(', opend + 1, close, pairs)
+        if close < opend:
+            print_parenthesis(output + ')', opend, close + 1, pairs)
+
 
 print_parenthesis('', 0, 0, 3)
+
 
 # 3、根据字典，从一个抹去空格的字符串里面提取出全部单词组合，并且拼接成正常的句子:
 # 例如: 输入一个字符串："thisisanexample", 程序输出: "this is an example"
 
 
-def word_break(inputs:str, word_set:set, memorized:dict):
+def word_break(inputs: str, word_set: set, memorized: dict):
     """
     深度优先搜索，对单词字符串进行迭代，
     查看是否在字典集合中，找到后，对剩下的字符进行递归调用
@@ -99,8 +100,9 @@ def word_break(inputs:str, word_set:set, memorized:dict):
     memorized[inputs] = res
     return res
 
+
 # test
 ins = 'thisisanexample'
-sd = {'this', 'is', 'an','example', }
+sd = {'this', 'is', 'an', 'example', }
 d = {}
 print(word_break(ins, sd, d))
--- Answers/week12/job12.py
+++ Answers/week12/job12.py
@@ -7,35 +7,38 @@ class IterC:
     def __iter__(self):
         yield from self.lst
 
+
 # test
 iter_c = IterC()
-iter_c.lst.extend([2,3,4,5])
+iter_c.lst.extend([2, 3, 4, 5])
 for c in iter_c:
     print(c)
+
 
 # 2、给出一个无序的整型列表，找出最长连续元素序列的长度，时间复杂度要求在线性时间内。
 # 例如：
 #     输入：[8,1,9,3,2,4]，那么其最长连续序列是[1,2,3,4]，即输出长度为4
 
-def find_longest_consequence(array:list):
+def find_longest_consequence(array: list):
     if not array:
         return 0
     hashset = set(array)
     max_num = 0
 
     for i in array:
-        if i-1 not in hashset:
+        if i - 1 not in hashset:
             j = i
             while i in hashset:
-                i+=1
-            max_num = max(max_num, i-j)
+                i += 1
+            max_num = max(max_num, i - j)
 
     return max_num
 
 
 # test
-print('Q2:', 80*'-')
-print(find_longest_consequence([8,1,9,3,2,4,5]))
+print('Q2:', 80 * '-')
+print(find_longest_consequence([8, 1, 9, 3, 2, 4, 5]))
+
 
 # 3、输入一个字符串，求不含有重复字母的最长子串的长度
 # 例如：
@@ -49,12 +52,13 @@ def longest_substring_length(s: str):
     for i in range(length):
         k = s[i]
         if k in d and d.get(k) >= start:
-            start = d.get(k) +1
+            start = d.get(k) + 1
         d[k] = i
-        max_len = max(max_len, i-start+1)
+        max_len = max(max_len, i - start + 1)
 
     return max_len
 
+
 # test
-print('Q3:', 80*'-')
+print('Q3:', 80 * '-')
 print(longest_substring_length('aaaabbcdefusqq'))
