2fb0f335e2dba709452eb012b569476d0627ae33
gvanrossum <dbawww@126.com>
2018-07-02 07:24:49 +0800

    add answer

--- Answers/week12/job12.py
+++ Answers/week12/job12.py
@@ -59,67 +59,6 @@ def longest_substring_length(s: str):
 print('Q3:', 80*'-')
 print(longest_substring_length('aaaabbcdefusqq'))
 
-class Quan:
-
-    def __init__(self, storage_name):
-        self.storage_name = storage_name
-
-    def __get__(self, instance, owner):
-        print('__gggggeeet', self, instance, owner)
-        return instance.__dict__[self.storage_name]
-
-    def __set__(self, instance, value):
-        print('___ssseee', self, instance, value)
-        if value > 0:
-            instance.__dict__[self.storage_name] = value
-        else:
-            raise ValueError('value must be > 0')
-
-
-class LineItem:
-    weight = Quan('weight')
-    price = Quan('price')
-
-    print('wwwxxx', weight, price)
-    def __init__(self, desc, wei, pri):
-        print('will auto init method ----',)
-        self.age = Quan('age')
-
-        print('in age ', self.age)
-        self.desc = Quan('desc')
-        self.weight = wei
-        self.price = pri
-        print('first...', self.__dict__)
-
-    def subtotal(self):
-        return self.weight * self.price
-
-    def uim(self):
-        print('----fssssss')
-        # self.fs = self.desc
-        # self.fs = 3434
 
 
 
-
-lint = LineItem('asdppp', 10, 22)
-print(lint.__dict__,)
-print('prolsqwqw....',)
-# lint.uim()
-# print(lint.fs)
-print(lint.weight, LineItem.__dict__)
-
-src = 'abcd'
-
-length = len(src)
-
-r = 0
-
-for offset in range(0, length, 3):
-    if offset + 3 <= length:
-        triple =  src[offset: offset+3]
-    else:
-        triple = src[offset:]
-        r = 3 -len(triple)
-        triple = triple + '\x00' * r
-    print(triple, r)
